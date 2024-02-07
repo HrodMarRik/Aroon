d75c5f2dae73b827fed15e17b81fc2b512c7076f
Romaric Chanavat <romaric.chanavat@gmail.com>
2024-02-07 14:26:52 +0100

    auto-up

---
+++ WEB/main.js
@@ -0,0 +1,7 @@
+// main.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Code JavaScript à exécuter après le chargement du document HTML
+
+    console.log("Le document HTML est entièrement chargé.");
+});
